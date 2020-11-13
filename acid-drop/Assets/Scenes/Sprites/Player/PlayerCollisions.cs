db9e14f9b2934a6ae12143a23e30daebee1bb2b2
nidnogg <henriquevt98@gmail.com>
2020-11-13 02:24:10 -0300

    unity stuff

---
+++ acid-drop/Assets/Scenes/Sprites/Player/PlayerCollisions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollisions : MonoBehaviour
+{   
+    public PlayerMovements movement;
+    public GameObject gameoverui;
+    public static bool gameispaused;
+    
+    public void NewGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+        gameispaused = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision .collider  .tag =="Obstacle")
+        {
+            
+            gameoverui.SetActive(true);
+            Time.timeScale = 0f;
+            gameispaused = true;
+        }
+    }
+
+}
---
+++ acid-drop/Assets/Scenes/Sprites/Player/PlayerMovements.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{   
+    public float speed; 
+    public float jumpforce;
+    private float moveinput;
+    public Rigidbody2D rb;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input .GetKeyDown (KeyCode.UpArrow))
+        {
+            rb.velocity = Vector2.up * jumpforce;
+            animator.SetBool("IsJumping", true);
+        }
+    } 
+    public void OnLanding() 
+    {
+        animator.SetBool("IsJumping", false);
+    }
+}
